8fd958a2024b771503c51cf3d42884bb94acd926
Oscar Saravia <oscarsaraviard0013@gmail.com>
2021-11-17 17:02:55 -0600

    fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
  
 app.get('*', (req, res) => {
-  res.sendFile(`./mindfulness-final/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+  res.sendFile(__dirname + '/index.html');
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
